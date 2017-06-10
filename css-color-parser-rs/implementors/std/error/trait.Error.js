17db6a51428efef6659728dac12549296df73815
Katkov Oleksandr <alexx.katkoff@gmail.com>
2017-06-10 14:22:40 +0300

    Updating docs for css-color-parser-rs v0.1.2

--- css-color-parser-rs/implementors/std/error/trait.Error.js
+++ css-color-parser-rs/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["css_color_parser"] = [];
+implementors["css_color_parser"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"css_color_parser/struct.ColorParseError.html\" title=\"struct css_color_parser::ColorParseError\">ColorParseError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
